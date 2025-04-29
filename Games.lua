4315ebddd7dce0b2195905e0845bb0ea35055926
SPEED.EXE <speedwave904@gmail.com>
2025-04-29 18:43:54 +0300

    Create Games.lua

---
+++ Games.lua
@@ -0,0 +1,7 @@
+local Games = loadstring(game:HttpGet("loadstring(game:HttpGet("https://raw.githubusercontent.com/speedwavevip/Speed_Aya/refs/heads/main/Games.lua"))()"))()
+
+for PlaceID, Execute in pairs(Games) do
+    if PlaceID == game.PlaceId then
+        loadstring(game:HttpGet(Execute))()
+    end
+end
