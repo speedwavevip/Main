44d914c8996713a071057db4f0b59271bf19ae9a
SPEED.EXE <speedwave904@gmail.com>
2025-07-31 05:00:25 +0300

    Update Games.lua

--- Games.lua
+++ Games.lua
@@ -1,4 +1,4 @@
-local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/speedwavevip/Speed_Aya/refs/heads/main/Brookhaven%20English.lua"))()
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/speedwavevip/Speed_Aya/refs/heads/main/Games.lua"))()
 
 for PlaceID, Execute in pairs(Games) do
     if PlaceID == game.PlaceId then
