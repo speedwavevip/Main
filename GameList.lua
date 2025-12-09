1941a68604cc811f74df69d6296857376127f6ba
SPEED.EXE <speedwave904@gmail.com>
2025-12-10 01:56:41 +0300

    Create GameList.lua

---
+++ GameList.lua
@@ -0,0 +1,6 @@
+local Games = {
+  [5523851880] = "https://raw.githubusercontent.com/speedwavevip/Speed_Aya/refs/heads/main/8BallpoolNew.lua",
+  [18668065416] = "https://raw.githubusercontent.com/speedwavevip/Speed_Aya/refs/heads/main/Blue%20lock.lua",
+
+}
+return Games
